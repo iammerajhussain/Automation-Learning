6c887a674950c788b11f8c6d8a88cb75472e28ad
iammerajhussain <meraj.hussain@successive.tech>
2026-05-11 18:07:56 +0530

    Input and Button type element Code

---
+++ tests/button.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test('TC01: Verify button is clickable and triggers action', async ({ page }) => {
+  await page.goto('https://www.qaplayground.com/practice');
+  await expect(page).toHaveTitle(/Practice Elements/);
+
+  await page.waitForTimeout(3000);
+
+  await page.getByTestId('practice-card-buttons').getByText('Practice Now').click();
+  await expect(page).toHaveTitle(/How to Handle Button Clicks/);
+
+  const button = page.getByRole('button', { name: 'Go To Home' });
+
+  await button.click();
+
+  await expect(page).toHaveURL('https://www.qaplayground.com/');
+
+});
+
+test('TC02: Verify button displays the correct label text', async ({ page }) => {
+  await page.goto('https://www.qaplayground.com/practice');
+  await expect(page).toHaveTitle(/Practice Elements/);
+
+    await page.getByTestId('practice-card-buttons').getByText('Practice Now').click();
+    await expect(page).toHaveTitle(/How to Handle Button Clicks/);
+
+    const button = page.getByRole('button', { name: 'Go To Home' });
+
+    await expect(button).toBeVisible();
+    await expect(button).toHaveText('Go To Home');
+
+});
+
+test('TC03: Verify button triggers the correct action on click', async ({ page }) => {
+  await page.goto('https://www.qaplayground.com/practice');
+  await expect(page).toHaveTitle(/Practice Elements/);
+
+    await page.getByTestId('practice-card-buttons').getByText('Practice Now').click();
+    await expect(page).toHaveTitle(/How to Handle Button Clicks/);
+
+    const button = page.getByRole('button', { name: 'Go To Home' });
+    await button.click();
+    await expect(page).toHaveURL('https://www.qaplayground.com/');
+
+});
---
+++ tests/input.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('TC01: Verify successful movie name input', async ({ page }) => {
+  await page.goto('https://www.qaplayground.com/practice');
+  await expect(page).toHaveTitle(/Practice Elements/);
+
+  await page.waitForTimeout(3000);
+
+  await page.getByTestId('practice-card-input-fields').getByText('Practice Now').click();
+  await expect(page).toHaveTitle(/Input Field Automation Practice/);
+
+  const inputLocator = page.locator(`//input[@name='movieName']`);
+  await inputLocator.fill('Inception');
+  
+  await expect(inputLocator).toHaveValue('Inception');
+
+});
+
+test('TC02: Verify input placeholder disappears on typing', async ({ page }) => {
+  await page.goto('https://www.qaplayground.com/practice');
+  await expect(page).toHaveTitle(/Practice Elements/);
+
+  await page.getByTestId('practice-card-input-fields').getByText('Practice Now').click();
+  await expect(page).toHaveTitle(/Input Field Automation Practice/);
+
+  const inputLocator = page.locator(`//input[@name='movieName']`);
+  await expect(inputLocator).toHaveAttribute('placeholder', 'Enter hollywood movie name');
+  
+  await inputLocator.fill('Inception');
+  
+await expect(inputLocator).toHaveValue('Inception');
+
+});
